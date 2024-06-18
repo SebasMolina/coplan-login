71619f370e1eaf574527c6995ec2c61ff7b864e7
Sebastian Molina <sebastian_molina@itrsa.com.ar>
2024-06-18 20:09:05 -0300

    cambios con tabs y routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from '@mainlayout/main-layout.component'
 
 export const routes: Routes = [
-  {
-    path:'',
-    component: MainLayoutComponent
-  }
+    { path: '', component: MainLayoutComponent },
+    { path: 'not-found', loadComponent: ()=> import('@not-found/not-found.component').then(res=>res.NotFoundComponent) }
 ];
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -2,18 +2,30 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RegisterComponent } from '@register/register.component';
 import { WelcomePageComponent } from '@welcome/welcome-page.component'
+import { LoginComponent } from '@login/login.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   imports: [
     RouterModule,
+    CommonModule,
     RegisterComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    LoginComponent
   ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
 export class MainLayoutComponent {
+
+  constructor () {}
+
+  currentTab: string = 'login';
+
+  selectTab(tab: string) {
+    this.currentTab = tab;
+  }
 
 }
