dd9d587590f59d8c37a917c2d21c9aadc093db21
Sebastian Molina <sebastian_molina@itrsa.com.ar>
2024-06-19 16:57:02 -0300

    nuevo componente olvide contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { MainLayoutComponent } from '@mainlayout/main-layout.component'
 
 export const routes: Routes = [
     { path: '', component: MainLayoutComponent },
-    { path: 'not-found', loadComponent: ()=> import('@not-found/not-found.component').then(res=>res.NotFoundComponent) }
+    { path: 'not-found', loadComponent: ()=> import('@not-found/not-found.component').then(res=>res.NotFoundComponent) },
+    { path: 'forgot-password', loadComponent: ()=> import('@forgot-password/forgot-password.component').then(res=>res.ForgotPasswordComponent) }
 ];
